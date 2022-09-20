9b71042af2ed70f78bfe8e5ec9eabab096673113
Agata Toczyńska <agata.toczynska@capgemini.com>
2022-09-20 09:01:26 +0200

    useRef example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import "./App.css";
+import { useRef } from "react";
 import { Parent } from "./components/Parent";
+import "./App.css";
 
 function App() {
+  const headerRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    headerRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="App">
-      <Parent />
+      <Parent headerRef={headerRef} />
       <div className="box violet"></div>
       <div className="box blue"></div>
       <div className="box yellow"></div>
       <div className="box red"></div>
+      <button onClick={handleClick} className="frame blue">
+        Scroll to the top
+      </button>
     </div>
   );
 }
--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -1,10 +1,20 @@
+import { RefObject } from "react";
 import { Child } from "./Child";
 
-export const Parent = () => {
+type ParentProps = {
+  headerRef: RefObject<HTMLDivElement>;
+};
+
+export const Parent: React.FC<ParentProps> = ({ headerRef }) => {
   return (
-    <div className="frame violet">
-      Parent
-      <Child />
-    </div>
+    <>
+      <div ref={headerRef} className="frame yellow">
+        Header
+      </div>
+      <div className="frame violet">
+        Parent
+        <Child />
+      </div>
+    </>
   );
 };
